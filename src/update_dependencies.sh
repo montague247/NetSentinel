a22c1727a0beff390a5cf8d8911571e38c73d6b1
montague247@github.com <dirk.helbig@volkswagen.de>
2025-04-15 19:10:17 +0200

    Extended deps update script

--- src/update_dependencies.sh
+++ src/update_dependencies.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
+#  _   _      _    _____            _   _            _ 
+# | \ | |    | |  / ____|          | | (_)          | |
+# |  \| | ___| |_| (___   ___ _ __ | |_ _ _ __   ___| |
+# | . ` |/ _ \ __|\___ \ / _ \ '_ \| __| | '_ \ / _ \ |
+# | |\  |  __/ |_ ____) |  __/ | | | |_| | | | |  __/ |
+# |_| \_|\___|\__|_____/ \___|_| |_|\__|_|_| |_|\___|_|
+#
+# Copyright (c) Dirk Helbig. All rights reserved.
+#
+
+# Stop script on NZEC
+set -e
+# Stop script if unbound variable found (use ${var:-} if intentional)
+set -u
+# By default cmd1 | cmd2 returns exit code of cmd2 regardless of cmd1 success
+# This is causing it to fail
+set -o pipefail
 
 echo "Update tool..."
 
