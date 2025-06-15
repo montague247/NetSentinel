00e886dd31c0caade819fed5f58343745ecb5d64
montague247@github.com <dirk.helbig@volkswagen.de>
2025-06-15 22:10:20 +0200

    Prevent access denied

--- src/NetSentinel.Automation/ZigbeeGateway.cs
+++ src/NetSentinel.Automation/ZigbeeGateway.cs
@@ -52,33 +52,68 @@ public static class ZigbeeGateway
     {
         var appPath = "/opt/zigbee2mqtt";
 
+        Log.Information("Ensuring Zigbee2MQTT application at {Path}", appPath);
+
         if (Directory.Exists(appPath))
             Shell.SudoExecute("git", "/opt", ["update", "https://github.com/Koenkk/zigbee2mqtt.git"], shellOptions);
         else
             Shell.SudoExecute("git", "/opt", ["clone", "https://github.com/Koenkk/zigbee2mqtt.git"], shellOptions);
 
+        Log.Information("Zigbee2MQTT application ensured at {Path}", appPath);
+
         Shell.Execute("npm", appPath, ["ci"]);
 
+        var tempConfigurationPath = Path.GetFullPath("Zigbee2MQTT-configuration.yaml");
         var configurationPath = Path.Combine(appPath, "data", "configuration.yaml");
 
-        if (File.Exists(configurationPath))
+        if (File.Exists(tempConfigurationPath))
             Log.Information("Zigbee2MQTT configuration already exists at {Path}", configurationPath);
         else
         {
-            Log.Information("Creating Zigbee2MQTT configuration at {Path}", configurationPath);
-            var defaultConfig = $@"mqtt:
-  base_topic: zigbee2mqtt
-  server: 'mqtt://localhost'
-  user: '{SecurityExtensions.CreateSecurePassword(16)}'
-  password: '{SecurityExtensions.CreateSecurePassword(32)}'
+            Log.Information("Creating Zigbee2MQTT configuration at {Path}", tempConfigurationPath);
+
+            var defaultConfig = $@"# Indicates the configuration version (used by configuration migrations)
+version: 4
+
+# Home Assistant integration (MQTT discovery)
+homeassistant:
+    enabled: false
+
+# Enable the frontend, runs on port 8080 by default
+frontend:
+    enabled: true
+
+# MQTT settings
+mqtt:
+    # MQTT base topic for zigbee2mqtt MQTT messages
+    base_topic: zigbee2mqtt
+    # MQTT server URL
+    server: 'mqtt://localhost'
+    # MQTT server authentication
+    user: '{SecurityExtensions.CreateSecurePassword(16)}'
+    password: '{SecurityExtensions.CreateSecurePassword(32)}'
 
 serial:
-  port: '/dev/ttyAMA0'  # Port for RaspBee II
-  adapter: 'deconz'  # Adapter for RaspBee II
-frontend:
-  port: 8080";
+    port: '/dev/ttyAMA0'  # Port for RaspBee II
+    adapter: 'deconz'  # Adapter for RaspBee II
 
-            File.WriteAllText(configurationPath, defaultConfig);
+# Periodically check whether devices are online/offline
+availability:
+    enabled: true
+
+# Advanced settings
+advanced:
+    # channel: 11
+    # Let Zigbee2MQTT generate a network key on first start
+    network_key: GENERATE
+    # Let Zigbee2MQTT generate a pan_id on first start
+    pan_id: GENERATE
+    # Let Zigbee2MQTT generate a ext_pan_id on first start
+    ext_pan_id: GENERATE";
+
+            File.WriteAllText(tempConfigurationPath, defaultConfig);
+
+            Shell.SudoExecute("cp", [tempConfigurationPath, configurationPath], shellOptions);
 
             Log.Information("Zigbee2MQTT configuration created successfully at {Path}", configurationPath);
         }
