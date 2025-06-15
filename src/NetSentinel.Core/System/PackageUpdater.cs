421ee7d52d6059159881396ab33b2f8ae466d857
montague247@github.com <dirk.helbig@volkswagen.de>
2025-06-15 19:59:17 +0200

    Use apt-get to prevent error messages

--- src/NetSentinel.Core/System/PackageUpdater.cs
+++ src/NetSentinel.Core/System/PackageUpdater.cs
@@ -5,6 +5,8 @@ namespace NetSentinel.System;
 
 public static class PackageUpdater
 {
+    private const string AptCommand = "apt-get";
+
     public static void Execute(IShellOptions options)
     {
         // Check if the system is running on a Debian-based distribution
@@ -24,25 +26,25 @@ public static class PackageUpdater
         var startedUtc = DateTime.UtcNow;
 
         Log.Information("Ensure that the necessary tools are installed");
-        Shell.SudoExecute("apt", ["update"], options);
+        Shell.SudoExecute(AptCommand, ["update"], options);
 
         Log.Information("List upgradable packages");
-        Shell.Execute("apt", ["list", "--upgradable"]);
+        Shell.Execute(AptCommand, ["list", "--upgradable"]);
 
         Log.Information("Upgrade packages");
-        Shell.SudoExecute("apt", ["upgrade", "-y"], options);
+        Shell.SudoExecute(AptCommand, ["upgrade", "-y"], options);
 
         Log.Information("Perform a full distribution upgrade");
-        Shell.SudoExecute("apt", ["dist-upgrade", "-y"], options);
+        Shell.SudoExecute(AptCommand, ["dist-upgrade", "-y"], options);
 
         Log.Information("Remove unused packages and clean up");
-        Shell.SudoExecute("apt", ["autoremove", "-y"], options);
+        Shell.SudoExecute(AptCommand, ["autoremove", "-y"], options);
 
         Log.Information("Clean up package cache");
-        Shell.SudoExecute("apt", ["autoclean"], options);
+        Shell.SudoExecute(AptCommand, ["autoclean"], options);
 
         Log.Information("Clean up any residual package files");
-        Shell.SudoExecute("apt", ["clean"], options);
+        Shell.SudoExecute(AptCommand, ["clean"], options);
 
         Log.Information("System packages updated successfully after {Duration}", DateTime.UtcNow - startedUtc);
 
